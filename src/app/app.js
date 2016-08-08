804691200a219a842abbd446f522922fedfe3a5f
vu <vik.uhryn@gmail.com>
2016-08-08 14:06:58 +0300

    first commit

---
+++ src/app/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './app.scss';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <h1>It works!!! Yeah!!!</h1>
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: './src/main',
+    output: {
+        path: 'dev/',
+        filename: 'app.js'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|dev)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loaders: [ 'style', 'css', 'sass' ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+};
